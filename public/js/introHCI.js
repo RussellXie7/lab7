f6f657642882ebcf5a082d2746f6d27ed9b9692c
RussellXie7 <russellxie@outlook.com>
2017-02-23 11:49:33 -0800

    google ana

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,9 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$(".likeBtn").click(sendGoogle);
+}
+
+function sendGoogle(e){
+	ga('send', 'event', 'like','click');
 }
